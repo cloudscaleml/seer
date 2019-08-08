4710e832bd79ccc52249f9161062b283062b2653
Damian Brady <info@damianbrady.com.au>
2019-08-08 11:34:38 +1000

    Updated pipeline with args passing to python

--- setup_pipeline.py
+++ setup_pipeline.py
@@ -6,140 +6,171 @@ from azureml.pipeline.core import Pipeline, PipelineData, PipelineParameter
 from azureml.pipeline.steps import PythonScriptStep, EstimatorStep
 from azureml.widgets import RunDetails
 from azureml.train.estimator import Estimator
-import os
+import sys, getopt
 
-# Get environment variables
-datastorename=os.environ['datastorename']
-containername=os.environ['containername']
-accountname=os.environ['accountname']
-accountkey=os.environ['storageaccountkey']
-datastorepath=os.environ['datapath']
-computetarget=os.environ['computetarget']
+def printhelp():
+        print ('Arguments:')
+        print ('  -d    Data Store name')
+        print ('  -dc   Data Store Container name')
+        print ('  -p    Data Store Path')
+        print ('  -a    Storage Account name')
+        print ('  -k    Storage Account Key')
+        print ('  -c    Compute Target name')
 
-print("Azure ML SDK Version: ", azureml.core.VERSION)
-
-# workspace
-ws = Workspace.from_config(
-    path='./azureml-config.json')
-ws.datastores
-
-# data
-datastore = ws.datastores[datastorename]
-if datastore is None:
-    datastore = Datastore.register_azure_blob_container(workspace=ws, 
-                                                datastore_name=datastorename, 
-                                                container_name=containername,
-                                                account_name=accountname, 
-                                                account_key=accountkey,
-                                                create_if_not_exists=False)
-
-
-# compute target
-compute = ws.compute_targets[computetarget]
-
-# # Define Pipeline!
-# The following will be created and then run:
-# 1. Pipeline Parameters
-# 2. Data Fetch Step
-# 3. Data Process Step
-# 4. Model Registration Step
-# 5. Training Step
-# 
-
-# ## Pipeline Parameters
-# We need to tell the Pipeline what it needs to learn to see!
-
-datapath = DataPath(datastore=datastore, path_on_datastore='hardware')
-data_path_pipeline_param = (PipelineParameter(name="data", 
-                                             default_value=datastorepath), 
-                                             DataPathComputeBinding(mode='mount'))
-data_path_pipeline_param
+def main(argv):
+    try:
+        opts, args = getopt.getopt(argv,"d:dc:p:a:k:c:")
+    except getopt.GetoptError:
+        printhelp
+    for opt, arg in opts:
+        if opt == '-h':
+            printhelp
+        elif opt == '-d':
+            datastorename = arg
+        elif opt == '-dc':
+            containername = arg
+        elif opt == '-p':
+            datastorepath = arg
+        elif opt == '-a':
+            accountname = arg
+        elif opt == '-k':
+            accountkey = arg
+        elif opt == '-c':
+            computetarget = arg
 
 
-# ## Data Process Step
+    # Get environment variables
+    #datastorename=os.environ['datastorename']
+    #containername=os.environ['containername']
+    #datastorepath=os.environ['datapath']
+    #accountname=os.environ['accountname']
+    #accountkey=os.environ['storageaccountkey']
+    #computetarget=os.environ['computetarget']
 
-seer_tfrecords = PipelineData(
-    "tfrecords_set",
-    datastore=datastore,
-    is_directory=True
-)
+    print("Azure ML SDK Version: ", azureml.core.VERSION)
 
-prep = Estimator(source_directory='.',
-                      compute_target=compute,
-                      entry_script='parse.py',
-                      use_gpu=True,
-                      pip_requirements_file='requirements.txt')
+    # workspace
+    ws = Workspace.from_config(
+        path='./azureml-config.json')
+    ws.datastores
 
-prepStep = EstimatorStep(
-    name='Data Preparation',
-    estimator=prep,
-    estimator_entry_script_arguments=["--source_path", data_path_pipeline_param, 
-                                      "--target_path", seer_tfrecords],
-    inputs=[data_path_pipeline_param],
-    outputs=[seer_tfrecords],
-    compute_target=compute
-)
+    # data
+    datastore = ws.datastores[datastorename]
+    if datastore is None:
+        datastore = Datastore.register_azure_blob_container(workspace=ws, 
+                                                    datastore_name=datastorename, 
+                                                    container_name=containername,
+                                                    account_name=accountname, 
+                                                    account_key=accountkey,
+                                                    create_if_not_exists=False)
 
-# ## Training Step
 
-seer_training = PipelineData(
-    "train",
-    datastore=datastore,
-    is_directory=True
-)
+    # compute target
+    compute = ws.compute_targets[computetarget]
 
-train = Estimator(source_directory='.',
-                      compute_target=compute,
-                      entry_script='train.py',
-                      use_gpu=True,
-                      pip_requirements_file='requirements.txt')
+    # # Define Pipeline!
+    # The following will be created and then run:
+    # 1. Pipeline Parameters
+    # 2. Data Fetch Step
+    # 3. Data Process Step
+    # 4. Model Registration Step
+    # 5. Training Step
+    # 
 
-trainStep = EstimatorStep(
-    name='Model Training',
-    estimator=train,
-    estimator_entry_script_arguments=["--source_path", seer_tfrecords, 
-                                      "--target_path", seer_training,
-                                      "--epochs", 5,
-                                      "--batch", 10,
-                                      "--lr", 0.001],
-    inputs=[seer_tfrecords],
-    outputs=[seer_training],
-    compute_target=compute
-)
+    # ## Pipeline Parameters
+    # We need to tell the Pipeline what it needs to learn to see!
 
-# # Register Model Step
+    datapath = DataPath(datastore=datastorepath, path_on_datastore='hardware')
+    data_path_pipeline_param = (PipelineParameter(name="data", 
+                                                default_value=datapath), 
+                                                DataPathComputeBinding(mode='mount'))
+    data_path_pipeline_param
 
-seer_model = PipelineData(
-    "model",
-    datastore=datastore,
-    is_directory=True
-)
 
-register = Estimator(source_directory='.',
-                      compute_target=compute,
-                      entry_script='register.py',
-                      use_gpu=True)
+    # ## Data Process Step
 
-registerStep = EstimatorStep(
-    name='Model Registration',
-    estimator=register,
-    estimator_entry_script_arguments=["--source_path", seer_training, 
-                                      "--target_path", seer_model],
-    inputs=[seer_training],
-    outputs=[seer_model],
-    compute_target=compute
-)
+    seer_tfrecords = PipelineData(
+        "tfrecords_set",
+        datastore=datastore,
+        is_directory=True
+    )
 
-# ## Create and publish the Pipeline
+    prep = Estimator(source_directory='.',
+                        compute_target=compute,
+                        entry_script='parse.py',
+                        use_gpu=True,
+                        pip_requirements_file='requirements.txt')
 
-pipeline = Pipeline(workspace=ws, steps=[prepStep, trainStep, registerStep])
+    prepStep = EstimatorStep(
+        name='Data Preparation',
+        estimator=prep,
+        estimator_entry_script_arguments=["--source_path", data_path_pipeline_param, 
+                                        "--target_path", seer_tfrecords],
+        inputs=[data_path_pipeline_param],
+        outputs=[seer_tfrecords],
+        compute_target=compute
+    )
 
-published_pipeline = pipeline.publish(
-    name="Seer Pipeline", 
-    description="Transfer learned image classifier. Uses folders as labels.")
+    # ## Training Step
 
-# Submit the pipeline to be run
-pipeline_run = Experiment(ws, 'seer').submit(pipeline)
-#RunDetails(pipeline_run).show()
+    seer_training = PipelineData(
+        "train",
+        datastore=datastore,
+        is_directory=True
+    )
 
-print('Run created with ID: ', pipeline_run.run_id)
+    train = Estimator(source_directory='.',
+                        compute_target=compute,
+                        entry_script='train.py',
+                        use_gpu=True,
+                        pip_requirements_file='requirements.txt')
+
+    trainStep = EstimatorStep(
+        name='Model Training',
+        estimator=train,
+        estimator_entry_script_arguments=["--source_path", seer_tfrecords, 
+                                        "--target_path", seer_training,
+                                        "--epochs", 5,
+                                        "--batch", 10,
+                                        "--lr", 0.001],
+        inputs=[seer_tfrecords],
+        outputs=[seer_training],
+        compute_target=compute
+    )
+
+    # # Register Model Step
+
+    seer_model = PipelineData(
+        "model",
+        datastore=datastore,
+        is_directory=True
+    )
+
+    register = Estimator(source_directory='.',
+                        compute_target=compute,
+                        entry_script='register.py',
+                        use_gpu=True)
+
+    registerStep = EstimatorStep(
+        name='Model Registration',
+        estimator=register,
+        estimator_entry_script_arguments=["--source_path", seer_training, 
+                                        "--target_path", seer_model],
+        inputs=[seer_training],
+        outputs=[seer_model],
+        compute_target=compute
+    )
+
+    # ## Create and publish the Pipeline
+
+    pipeline = Pipeline(workspace=ws, steps=[prepStep, trainStep, registerStep])
+
+    published_pipeline = pipeline.publish(
+        name="Seer Pipeline", 
+        description="Transfer learned image classifier. Uses folders as labels.")
+
+    # Submit the pipeline to be run
+    pipeline_run = Experiment(ws, 'seer').submit(pipeline)
+    #RunDetails(pipeline_run).show()
+
+    print('Run created with ID: ', pipeline_run.run_id)
