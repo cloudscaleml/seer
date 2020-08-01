c0b72707512c1eb4ed9eee0687915fdad9e3b1b2
sethjuarez <me@sethjuarez.com>
2020-07-31 18:19:01 -0700

    corrected amlcallback to match latest keras api

--- amlcallback.py
+++ amlcallback.py
@@ -5,27 +5,18 @@ class AMLCallback(tf.keras.callbacks.Callback):
 
     def __init__(self, run):
         self.run = run
-        self.offline = self.run.id.startswith('OfflineRun')
-        if self.offline:
-            print('Offline AML Context: {}'.format(self.offline))
-        else:
-            print('Online AML Context: {}'.format(run))
 
-    def set_params(self, params):
-        self.params = params
-        self.metrics = self.params['metrics']
-
-    def set_model(self, model):
-        self.model = model
-
-    def on_train_batch_end(self, batch, logs=None):
-        if not self.offline:
-            for item in self.metrics:
-                if item in logs:
-                    self.run.log(item, logs[item])
+    def on_train_end(self, logs=None):
+        if logs != None:
+            for k in logs.keys():
+                self.run.log(f'final_{k}', logs[k])
 
     def on_epoch_end(self, epoch, logs=None):
-        if not self.offline:
-            for item in self.metrics:
-                if item in logs:
-                    self.run.log('epoch_{}'.format(item), logs[item])
+        if logs != None:
+            for k in logs.keys():
+                self.run.log(f'epoch_{k}', logs[k])
+
+    def on_train_batch_end(self, batch, logs=None):
+        if logs != None:
+            for k in logs.keys():
+                self.run.log(f'epoch_{k}', logs[k])
